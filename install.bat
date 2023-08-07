a9efb8c596a724db306513bfae7c5dc1b6fb9207
pl4g <viniciuslucianocardoso@gmail.com>
2023-08-07 08:08:59 -0300

    updated install.bat

--- install.bat
+++ install.bat
@@ -2,8 +2,8 @@
 set "dir=%APPDATA%\Mozilla\Firefox\Profiles"
 if exist "%dir%" (
     for /D %%D in ("%dir%\*default-release*") do (
-        xcopy ".\chrome\" "%%D\chrome\" > nul
-        xcopy ".\user.js" "%%D" > nul
+        xcopy /e /k /h /i /q ".\chrome\" "%%D\chrome\"
+        xcopy /e /k /h /i /q ".\user.js" "%%D"
     )
     echo Firefox cloned to the directories:
     for /D %%D in ("%dir%\*default-release*") do echo %%~nxD
