61457847f02efae1834a7635fa44a174511dc5fa
pl4g <viniciuslucianocardoso@gmail.com>
2024-01-08 15:11:41 -0300

    Re-strutured the repo and updated the install scripts to work with it as well.

--- install.bat
+++ install.bat
@@ -2,12 +2,10 @@
 set "dir=%APPDATA%\Mozilla\Firefox\Profiles"
 if exist "%dir%" (
     for /D %%D in ("%dir%\*default-release*") do (
-        xcopy /e /k /h /i /q ".\chrome\" "%%D\chrome\"
-        xcopy /e /k /h /i /q ".\user.js" "%%D"
+        xcopy /e /k /h /i /q ".\soupfox-files\" "%%D\"
     )
     echo Firefox cloned to the directories:
     for /D %%D in ("%dir%\*default-release*") do echo %%~nxD
 )
 
 set "dir="
-
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ firefoxDir=~/.mozilla/firefox/
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -d "$firefoxDir" ] ; then
-   cp -r $scriptDir/chrome/ $scriptDir/user.js $firefoxDir?*.default-release
+   cp -r $scriptDir/soupfox-files/ $firefoxDir?*.default-release
    echo -e "Firefox cloned to the directories:\n$(ls -a $firefoxDir | grep 'default-release')"
 fi
 
