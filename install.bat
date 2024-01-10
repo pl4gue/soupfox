505e3be99531b2ccd44f3fb55b12b058c41560b1
pl4gue <viniciuslucianocardoso@gmail.com>
2024-01-10 15:34:34 -0300

    windows fix

--- install.bat
+++ install.bat
@@ -1,11 +1,11 @@
 @echo off
 set "dir=%APPDATA%\Mozilla\Firefox\Profiles"
 if exist "%dir%" (
-    for /D %%D in ("%dir%\*default-release*") do (
+    for /D %%D in ("%dir%\*default*") do (
         xcopy /e /k /h /i /q ".\soupfox-files\" "%%D\"
     )
     echo Firefox cloned to the directories:
-    for /D %%D in ("%dir%\*default-release*") do echo %%~nxD
+    for /D %%D in ("%dir%\*default*") do echo %%~nxD
 )
 
 set "dir="
