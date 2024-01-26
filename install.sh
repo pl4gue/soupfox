85671d5686f7798d9dde82e329711815760bf8a3
plague <viniciuslucianocardoso@gmail.com>
2024-01-26 14:05:19 -0300

    update to the install script on linux

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@ firefoxDir=~/.mozilla/firefox/
 scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -d "$firefoxDir" ] ; then
-   cp -r $scriptDir/soupfox-files/ $firefoxDir?*.default-release
-   echo -e "Firefox cloned to the directories:\n$(ls -a $firefoxDir | grep 'default-release')"
+   for dir in $(ls $firefoxDir | grep default);
+	do
+	   cp -r $scriptDir/soupfox-files/* $firefoxDir$dir
+	done
+   echo -e "Firefox cloned to the directories:\n$(ls -a $firefoxDir | grep 'default')"
 fi
 
 unset scriptDir
